e41eb7734543fde79d6a6360e57ed2057a52ac03
gulkayir1 <de.gulkayir@gmail.com>
2021-11-11 10:08:39 +0600

    changed temlplates second

--- fitnessblog/settings.py
+++ fitnessblog/settings.py
@@ -4,14 +4,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from decouple import config
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET KEY')
+SECRET_KEY = 'django-insecure-b^ro7@hne-7i4&#zk_c)h4*!!@&ze089)#q5$04u=qx84q+kyr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'fitnessblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
@@ -126,8 +126,10 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'media')
 )
+
+AUTH_USER_MODEL = 'account.User'
 
 
 # Default primary key field type
--- main/models.py
+++ main/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+class Category(models.Model):
+    slug = models.SlugField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=55)
+    image = models.ImageField(blank=True, null=True, upload_to='categories')
+    parent = models.ForeignKey('self', related_name='children', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        if self.parent:
+            return f'{self.name} --> {self.parent}'
+        return self.name
+
+    @property
+    def get_children(self):
+        if self.children:
+            return self.children.all()
+        return False
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    cooking_time = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='articles')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='articles', blank=True)
+    recipe = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        if self.image:
+            return self.image.url
+        return ''
